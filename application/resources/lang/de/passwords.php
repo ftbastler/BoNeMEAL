f6a8bfc5e12c28a34d338f538d68b0197c39e33b
ftbastler <user@server.end>
2015-05-11 21:54:38 +0200

    Add German translation
    Thanks to @Townville

---
+++ application/resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passwort muss mindestens 6 Zeichen lang sein.",
+	"user" => "Wir konnten keinen Benutzer mit der E-Mail Adresse finden.",
+	"token" => "Dieser Password Zurücksetzen Schlüssel ist falsch.",
+	"sent" => "Wir haben dir ein Zurücksetzungslink geschickt!",
+	"reset" => "Dein Passwort wurde zurückgesetzt!",
+
+];
