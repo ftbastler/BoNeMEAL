ace91f56096e973611966e9bf10d84e1cca0acb9
ftbastler <user@server.end>
2015-05-22 15:46:18 +0200

    Add some more stats

--- application/app/Http/Controllers/StatsController.php
+++ application/app/Http/Controllers/StatsController.php
@@ -15,13 +15,30 @@ class StatsController extends Controller {
 			$activeBans = null;
 			$activeMutes = null;
 
+			$numBans = 0;
+			$numMutes = 0;
+			$numKicks = 0;
+			$numWarnings = 0;
+			$numIpBans = 0;
+
 			foreach($servers as $server) {
 				$players[$server->id] = \App\Player::on($server->id)->count();
 				$activeBans[$server->id] = \App\PlayerBan::on($server->id)->active()->count();
 				$activeMutes[$server->id] = \App\PlayerMute::on($server->id)->active()->count();
+
+				$numBans += \App\PlayerBan::on($server->id)->count();
+				$numBans += \App\PlayerBanRecord::on($server->id)->count();
+				$numMutes += \App\PlayerMute::on($server->id)->count();
+				$numMutes += \App\PlayerMuteRecord::on($server->id)->count();
+				$numKicks += \App\PlayerKick::on($server->id)->count();
+				$numWarnings += \App\PlayerWarning::on($server->id)->count();
+				$numIpBans += \App\IpBan::on($server->id)->count();
+				$numIpBans += \App\IpBanRecord::on($server->id)->count();
 			}
 
-			return compact('servers', 'players', 'activeMutes', 'activeBans');
+			$nums = compact('numBans', 'numMutes', 'numKicks', 'numWarnings', 'numIpBans');
+
+			return compact('servers', 'players', 'activeMutes', 'activeBans', 'nums');
 		});
 
 		return view('stats', $data);
--- application/resources/views/stats.blade.php
+++ application/resources/views/stats.blade.php
@@ -4,17 +4,24 @@
 <div class="container">
 	<h2 class="page-header" style="margin-top: 0px;">{{ trans('app.statistics') }}</h2>
 	
+	<h3>{{ trans('app.generalStats') }}</h3>
+
+	<canvas id="myChartBars" style="min-width: 100%" height="90"></canvas>
+
+	<br />
+	<h3>{{ trans('app.serverStats') }}</h3>
+
 	<div class="row">
 		@foreach($servers as $server)
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<div class="caption">
-						<h3 style="margin-top: 0px">{{ $server->name }}</h3>
-						<br />
-						<canvas id="myChart{{ $server->id }}" style="min-width: 100%" height="150"></canvas>
-					</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<div class="caption">
+					<h3 style="margin-top: 0px">{{ $server->name }}</h3>
+					<br />
+					<canvas id="myChart{{ $server->id }}" style="min-width: 100%" height="150"></canvas>
 				</div>
 			</div>
+		</div>
 		@endforeach
 	</div>
 </div>
@@ -22,20 +29,41 @@
 
 @section('scripts')
 <script>
-$(document).ready(function() {
-	var options = {
+	$(document).ready(function() {
+		var options = {
 			segmentShowStroke : true,
 			segmentStrokeColor : "#fff",
 			segmentStrokeWidth : 2,
 			percentageInnerCutout : 40, // This is 0 for Pie charts
-			animationSteps : 100,
-			animationEasing : "easeInOutQuint",
-			animateRotate : true,
-			animateScale : true,
+			animateRotate : false,
+			animateScale : false,
 			pointHitDetectionRadius : 5
-	};
+		};
 
-	@foreach($servers as $server)
+		var dataBars = {
+			labels: [
+					"{{ ucfirst(Lang::choice('app.choice.ban', $nums['numBans'])) }}",
+					"{{ ucfirst(Lang::choice('app.choice.ipBan', $nums['numIpBans'])) }}",
+					"{{ ucfirst(Lang::choice('app.choice.mute', $nums['numMutes'])) }}",
+					"{{ ucfirst(Lang::choice('app.choice.kick', $nums['numKicks'])) }}",
+					"{{ ucfirst(Lang::choice('app.choice.warning', $nums['numWarnings'])) }}"
+				],
+			datasets: [
+			{
+				label: "",
+				fillColor: "rgba(220,220,220,0.5)",
+				strokeColor: "rgba(220,220,220,0.8)",
+				highlightFill: "rgba(220,220,220,0.75)",
+				highlightStroke: "rgba(220,220,220,1)",
+				data: [{{ $nums['numBans'] }}, {{ $nums['numIpBans'] }}, {{ $nums['numMutes'] }}, {{ $nums['numKicks'] }}, {{ $nums['numWarnings'] }}]
+			}
+			]
+		};
+
+		var ctxBars = document.getElementById("myChartBars").getContext("2d");
+		new Chart(ctxBars).Bar(dataBars, options);
+
+		@foreach($servers as $server)
 		var data{{ $server->id }} = [
 		{
 			value: {{ $activeBans[$server->id] }},
@@ -56,7 +84,7 @@ $(document).ready(function() {
 
 		var ctx{{ $server->id }} = document.getElementById("myChart{{ $server->id }}").getContext("2d");
 		new Chart(ctx{{ $server->id }}).Pie(data{{ $server->id }}, options);
-	@endforeach
-});
+		@endforeach
+	});
 </script>
 @endsection
